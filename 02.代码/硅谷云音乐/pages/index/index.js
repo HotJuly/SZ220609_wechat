52f9f336f2b5b2a59594fed6ea407feb47065942
HotJuly <949588380@qq.com>
2022-12-14 10:55:59 +0800

    25.小程序发送请求注意事项

--- 02.代码/硅谷云音乐/pages/index/index.js
+++ 02.代码/硅谷云音乐/pages/index/index.js
@@ -5,13 +5,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    banners:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    /*
+      发送请求的三个问题:
+        1.在哪发
+          在Vue中,created或者mounted中都可以发送请求
+          小程序中的onLoad相当于是created
+          所以此处选择使用onLoad
+
+        2.怎么发
+          在Vue项目中,会使用axios发送请求
+            axios其实就是ajax+promise组成的
+
+            注意:小程序没有遵守W3C的标准,没有BOM和DOM
+                所以小程序没办法发送ajax请求,导致没办法使用axios
+
+            小程序的全局对象是wx,没有window
+
+          小程序中使用API:wx.request
+            注意:小程序没有proxy代理规则,url路径必须书写完整
+
+        3.往哪发
+          看接口文档
+            接口文档三要素:
+              1.请求路径
+              2.请求方式
+              3.请求参数
+    
+    */
+    // console.log('window',window)
+    wx.request({
+      url:"http://localhost:3000/banner",
+      data:{
+        type:2
+      },
+      success:(res)=>{
+        // console.log('/banner',res)
+        this.setData({
+          banners:res.data.banners
+        })
+      }
+    })
 
   },
 
